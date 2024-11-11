832fd0bd6cc7d9e719672bbb9205b02fef00c0b3
iridiium <60609879+iridiium@users.noreply.github.com>
2024-11-11 21:49:08 +0000

    added sprites to the board

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# Paste pre-Pyrfected Python
-import pygame
-
-# Constants
-BLACK = (0, 0, 0)
-GREY = (128, 128, 128)
-WHITE = (255, 255, 255)
+import pygame, random
 
 
-# Background
+# Image classes
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
@@ -16,19 +10,57 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
+    def change_image(self, new_image_file):
+        self.image = pygame.image.load(new_image_file)
 
-# Tile
+
+class Spritesheet:
+    def __init__(self, image_file, sprite_dimensions):
+        self.image = pygame.image.load(image_file)
+        self.sprite_dimensions = sprite_dimensions
+
+    def get_sprite(self, sprite_position):
+        return self.image.subsurface(
+            pygame.Rect(
+                sprite_position[0] * self.sprite_dimensions[0],
+                sprite_position[1] * self.sprite_dimensions[1],
+                self.sprite_dimensions[0],
+                self.sprite_dimensions[1],
+            )
+        )
+
+    def get_random_sprite(self):
+        sprites_in_row = self.image.get_width() / self.sprite_dimensions[0]
+        sprites_in_column = self.image.get_height() / self.sprite_dimensions[1]
+
+        return self.image.subsurface(
+            pygame.Rect(
+                random.randint(0, int(sprites_in_row) - 1)
+                * self.sprite_dimensions[0],
+                random.randint(0, int(sprites_in_column) - 1)
+                * self.sprite_dimensions[1],
+                self.sprite_dimensions[0],
+                self.sprite_dimensions[1],
+            )
+        )
+
+
+# Board classes
 class Tile:
 
-    def __init__(self, x_pos, y_pos, colour, size, border_size):
+    def __init__(self, x_pos, y_pos, colour, image, size, border_size):
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.colour = colour
+        self.image = image
         self.size = size
         self.border_size = border_size
 
     def get_colour(self):
         return self.colour
+
+    def get_image(self):
+        return self.image
 
     def get_rect_object(self, x, y):
         return (
@@ -38,8 +70,19 @@ class Tile:
             self.size,
         )
 
+    def get_picture_rect_object(self, x, y):
+        return (
+            x * (self.size + self.border_size)
+            + self.x_pos
+            + self.border_size / 2,
+            y * (self.size + self.border_size)
+            + self.y_pos
+            + self.border_size / 2,
+            self.size,
+            self.size,
+        )
 
-# Board
+
 class Board:
 
     def __init__(
@@ -51,6 +94,7 @@ class Board:
         tile_colour,
         tile_size,
         tile_border_size,
+        spritesheet,
     ):
         self.num_in_row = num_in_row
         self.num_in_column = num_in_column
@@ -59,10 +103,18 @@ class Board:
         self.tile_colour = tile_colour
         self.tile_size = tile_size
         self.tile_border_size = tile_border_size
+        self.spritesheet = spritesheet
 
         self.rows = [
             [
-                Tile(x_pos, y_pos, tile_colour, tile_size, tile_border_size)
+                Tile(
+                    x_pos,
+                    y_pos,
+                    tile_colour,
+                    spritesheet.get_random_sprite(),
+                    tile_size,
+                    tile_border_size,
+                )
                 for _ in range(self.num_in_row)
             ]
             for _ in range(self.num_in_column)
@@ -72,7 +124,9 @@ class Board:
         for y, row in enumerate(self.rows):
             for x, cell in enumerate(row):
                 colour = cell.get_colour()
+
                 rect_object = cell.get_rect_object(x, y)
+                picture_rect_object = cell.get_picture_rect_object(x, y)
 
                 pygame.draw.rect(
                     window,
@@ -80,40 +134,75 @@ class Board:
                     rect_object,
                 )
 
+                window.blit(cell.get_image(), picture_rect_object)
+
+
+# Constants
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+WHITE = (255, 255, 255)
+
+WINDOW_SIZE = (800, 600)
+
+BOARD_SIZE = (5, 5)
+
+TILE_SIZE = 72
+TILE_BORDER_SIZE = 8
+TILE_TOTAL_SIZE = TILE_SIZE + TILE_BORDER_SIZE
+
+BOARD_POS = (
+    (WINDOW_SIZE[0] - BOARD_SIZE[0] * TILE_TOTAL_SIZE) / 2,
+    (WINDOW_SIZE[1] - BOARD_SIZE[1] * TILE_TOTAL_SIZE) / 2,
+)
+
 
 # Game loop
-WIDTH = 900
-HEIGHT = 675
+def main():
+    pygame.init()
+    window = pygame.display.set_mode(WINDOW_SIZE)
+    clock = pygame.time.Clock()
 
-pygame.init()
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+    bg = Background("back_900x675.png", (0, 0))
+    planets = Spritesheet(
+        "CelestialObjects/CelestialObjects_Planets.png", (64, 64)
+    )
 
-"""
-num_in_row, num_in_column, x_pos, y_pos
-tile_colour, tile_size, tile_border_size
-"""
-board = Board(8, 8, 142, 29.5, GREY, 70, 7)
+    """
+    num_in_row, num_in_column, x_pos, y_pos
+    tile_colour, tile_size, tile_border_size
+    """
+    board = Board(
+        BOARD_SIZE[0],
+        BOARD_SIZE[1],
+        BOARD_POS[0],
+        BOARD_POS[1],
+        GREY,
+        TILE_SIZE,
+        TILE_BORDER_SIZE,
+        planets,
+    )
 
-bg = Background("back_900x675.png", (0, 0))
+    running = True
+    while running:
+        clock.tick(100)
 
-running = True
-while running:
-    clock.tick(100)
+        pos = pygame.mouse.get_pos()
+        x, y = pos[0], pos[1]
 
-    pos = pygame.mouse.get_pos()
-    x, y = pos[0], pos[1]
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        window.fill(WHITE)
+        window.blit(bg.image, bg.rect)
 
-    window.fill(WHITE)
-    window.blit(bg.image, bg.rect)
+        board.draw_self(window)
 
-    board.draw_self(window)
+        pygame.display.flip()
 
-    pygame.display.flip()
+    pygame.quit()
+    exit()
 
-pygame.quit()
-exit()
+
+if __name__ == "__main__":
+    main()
